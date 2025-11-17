9cb035c5ff3d21f896c9b5284e1e2e1ac59e4aab
Maria Kathleen Joana Engalla <kathyengallaa@gmail.com>
2025-11-18 01:55:35 +0800

    added navigation bar on homepage

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./index.css";
+import logoSrc from "./assets/Light LocaleCafe logo.PNG";
 
 function Home() {
-    return (
-        <div>
-            <h1>Welcome to the Cafe!</h1>
-            <p>Enjoy our delicious menu and cozy atmosphere.</p>
-        </div>
-    )
+  return (
+    <>
+      <header className="header">
+        <a href="/" className="logo">
+          <img src={logoSrc} alt="LocaleCafe logo" className="header-logo" />
+        </a>
+
+        <nav className="navibar">
+          <p
+            id="for-the-selection-button-3"
+            className="for-the-selection-button-3"
+          ></p>
+          <a href="/">Home</a>
+          <a className="c-button" href="/logout">
+            <span className="label">Log Out</span>
+          </a>
+        </nav>
+      </header>
+
+      <main className="page-content">
+        <h1>Welcome to the Cafe!</h1>
+        <p>Enjoy our delicious menu and cozy atmosphere.</p>
+      </main>
+    </>
+  );
 }
 
-export default Home
+export default Home;
