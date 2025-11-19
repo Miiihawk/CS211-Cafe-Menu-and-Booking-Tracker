189f6c26df05d4015ba56a0c6e184ca0f963ceb2
Maria Kathleen Joana Engalla <kathyengallaa@gmail.com>
2025-11-19 16:11:46 +0800

    Tweak admin homepage

--- frontend/src/AdminHome.jsx
+++ frontend/src/AdminHome.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./admin_index.css";
+import logoSrc from "./assets/Light LocaleCafe logo.PNG";
+import homeBg from "./assets/homebg.png";
 
 function AdminHome() {
-    return (
-        <div>
-            <h1>Admin Home Page</h1>
-        </div>
-    )
+  return (
+    <>
+      <div
+        className="page-bg"
+        style={{
+          backgroundImage: `url(${homeBg})`,
+        }}
+      >
+        <header className="header">
+          <a href="/" className="logo">
+            <img src={logoSrc} alt="LocaleCafe logo" className="header-logo" />
+          </a>
+
+          <nav className="navibar">
+            <p
+              id="for-the-selection-button-3"
+              className="for-the-selection-button-3"
+            ></p>
+            <a href="/">Home</a>
+            <a className="c-button" href="/logout">
+              <span className="label">Log Out</span>
+            </a>
+          </nav>
+        </header>
+
+        <main className="page-content">
+          <h1>Welcome to the Cafe!</h1>
+          <p>Enjoy our delicious menu and cozy atmosphere.</p>
+        </main>
+      </div>
+    </>
+  );
 }
 
-export default AdminHome
+export default AdminHome;
