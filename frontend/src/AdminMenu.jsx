eca4f167ccf127e7737981d50a00383ac5367cb6
Maria Kathleen Joana Engalla <kathyengallaa@gmail.com>
2025-11-20 11:32:58 +0800

    added Admin pages

---
+++ frontend/src/AdminMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminMenu() {
+    return (
+        <div>
+            <h1>Admin Menu Page</h1>
+        </div>
+    )
+}
+
+export default AdminMenu
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home'
 import AdminHome from './AdminHome'
+import AdminMenu from './AdminMenu'
+import AdminBooking from './AdminBooking'
+import AdminGallery from './AdminGallery'
+import Home from './Home'
 import SignUp from './SignUp'
 import LogIn from './LogIn'
 import Menu from './Menu'
@@ -16,10 +19,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<AdminHome />} />
+        <Route path="/a_home" element={<AdminHome />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/a_menu" element={<AdminMenu />} />
+        <Route path="/a_booking" element={<AdminBooking />} />
+        <Route path="/a_gallery" element={<AdminGallery />} />
+
+
+        <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/booking" element={<Booking />} />
         <Route path="/gallery" element={<Gallery />} />
