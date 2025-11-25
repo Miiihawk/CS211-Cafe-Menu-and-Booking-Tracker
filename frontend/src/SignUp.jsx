1dbf1513b11655eb742215030c060fec04fe39cc
Maria Kathleen Joana Engalla <kathyengallaa@gmail.com>
2025-11-26 02:36:11 +0800

    fixed className in login and signup

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -51,7 +51,7 @@ export default function SignUp() {
           <div className="login-card">
             <h1 className="login-title">Sign Up</h1>
 
-            <label className="field-label">First Name</label>
+            <label className="login-field-label">First Name</label>
             <input
               className="text-input"
               type="text"
@@ -61,7 +61,7 @@ export default function SignUp() {
               onChange={(e) => setFirstName(e.target.value)}
             />
 
-            <label className="field-label">Last Name</label>
+            <label className="login-field-label">Last Name</label>
             <input
               className="text-input"
               type="text"
@@ -71,7 +71,7 @@ export default function SignUp() {
               onChange={(e) => setLastName(e.target.value)}
             />
 
-            <label className="field-label">Email</label>
+            <label className="login-field-label">Email</label>
             <input
               className="text-input"
               type="email"
@@ -81,7 +81,7 @@ export default function SignUp() {
               onChange={(e) => setEmail(e.target.value)}
             />
 
-            <label className="field-label">Phone Number</label>
+            <label className="login-field-label">Phone Number</label>
             <input
               className="text-input"
               type="tel"
@@ -91,7 +91,7 @@ export default function SignUp() {
               onChange={(e) => setPhone(e.target.value)}
             />
 
-            <label className="field-label">Password</label>
+            <label className="login-field-label">Password</label>
             <input
               className="text-input"
               type="password"
